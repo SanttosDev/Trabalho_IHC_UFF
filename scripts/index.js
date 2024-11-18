5ca3dee22dc0318832d867b424f726a260f45311
SanttosDev <josuels@id.uff.br>
2024-11-17 21:27:22 -0300

    ok

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,14 @@ const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
 
-
 button.addEventListener('click', function () {
   mobileNavbar.classList.toggle('active');
 });
-
 
 window.addEventListener('scroll', function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add('active');
   return navbar.classList.remove('active');
 });
-
 
 document.querySelectorAll('.navbar__links a, .mobile__links a').forEach(link => {
   link.addEventListener('click', function (e) {
@@ -40,3 +37,4 @@ document.querySelectorAll('.navbar__links a, .mobile__links a').forEach(link => 
       }
   });
 });
+
