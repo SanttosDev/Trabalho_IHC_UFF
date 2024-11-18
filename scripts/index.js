bddc919dec677636d3709d122b52c04bf300ef47
SanttosDev <josuels@id.uff.br>
2024-11-17 21:14:31 -0300

    rolagem

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
-const navbar = document.querySelector('.navbar');
-const mobileNavbar = document.querySelector('.navbar__mobile');
-const button = document.querySelector('.burguer');
 
-button.addEventListener('click', function () {
-  mobileNavbar.classList.toggle('active');
-});
 
-window.addEventListener('scroll', function () {
-  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
-  return navbar.classList.remove('active');
+document.querySelectorAll('.navbar__links a, .mobile__links a').forEach(link => {
+  link.addEventListener('click', function (e) {
+      e.preventDefault(); 
+
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+
+      if (targetElement) {
+          const offset = 100; 
+          const elementPosition = targetElement.offsetTop; 
+          const offsetPosition = elementPosition - offset; 
+
+          window.scrollTo({
+              top: offsetPosition,
+              behavior: 'smooth' 
+          });
+      }
+  });
 });
