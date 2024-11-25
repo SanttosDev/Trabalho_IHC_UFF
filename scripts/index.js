282b2efebcac3581619a9be70816ca8444b046ee
SanttosDev <josuels@id.uff.br>
2024-11-25 17:02:15 -0300

    Personas

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,27 @@
-const navbar = document.querySelector('.navbar');
-const mobileNavbar = document.querySelector('.navbar__mobile');
-const button = document.querySelector('.burguer');
-
-button.addEventListener('click', function () {
-  mobileNavbar.classList.toggle('active');
-});
-
-window.addEventListener('scroll', function () {
-  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
-  return navbar.classList.remove('active');
-});
-
 document.querySelectorAll('.navbar__links a, .mobile__links a').forEach(link => {
   link.addEventListener('click', function (e) {
-      e.preventDefault(); 
+    const href = this.getAttribute('href'); 
 
-      
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
+    if (!href.startsWith('#')) return; 
 
-     
-      if (targetElement) {
-          const offset = 100; 
-          const elementPosition = targetElement.offsetTop; 
-          const offsetPosition = elementPosition - offset; 
+    e.preventDefault();
 
-          window.scrollTo({
-              top: offsetPosition,
-              behavior: 'smooth' 
-          });
-      }
+    const targetId = href.substring(1);
+    const targetElement = document.getElementById(targetId);
 
-      
-      if (mobileNavbar.classList.contains('active')) {
-          mobileNavbar.classList.remove('active');
-      }
+    if (targetElement) {
+      const offset = 100;
+      const elementPosition = targetElement.offsetTop;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+
+    if (mobileNavbar.classList.contains('active')) {
+      mobileNavbar.classList.remove('active');
+    }
   });
 });
-
